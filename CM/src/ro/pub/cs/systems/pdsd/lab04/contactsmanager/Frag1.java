cf363656189feb963b4a704b8a5d95145d2618f6
bolov <mihail.balan@gmail.com>
2015-03-17 21:38:10 +0200

    interfata grafica

---
+++ CM/src/ro/pub/cs/systems/pdsd/lab04/contactsmanager/Frag1.java
@@ -0,0 +1,15 @@
+package ro.pub.cs.systems.pdsd.lab04.contactsmanager;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Frag1 extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater i1, ViewGroup vg, Bundle bdl) {
+		return i1.inflate(R.layout.f1, vg, false);
+	}
+
+}
